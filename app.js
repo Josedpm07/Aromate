6e80cce580d80d5d5502b37a96a1ea0be9054f4c
Josedpm07 <157835463+Josedpm07@users.noreply.github.com>
2026-08-16 00:59:28 -0400

    Remove payment modal functionality
    Removed payment modal event listeners and related code.

--- app.js
+++ app.js
@@ -136,16 +136,3 @@ currentProductSizes.forEach((size) => {
     size.style.color = "white";
   });
 });
-
-// Modal del formulario de pago
-const productButton = document.querySelector(".productButton");
-const payment = document.querySelector(".payment");
-const close = document.querySelector(".close");
-
-productButton.addEventListener("click", () => {
-  payment.style.display = "flex";
-});
-
-close.addEventListener("click", () => {
-  payment.style.display = "none";
-});
